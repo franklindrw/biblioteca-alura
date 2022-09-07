24ce4a2eebaa72efb4932882c0d7333bf6a99926
franklindrw <franklindrw@gmail.com>
2022-09-06 22:29:42 -0300

    adicionando CRUD na api

--- src/app.ts
+++ src/app.ts
@@ -16,15 +16,43 @@ const livros = [
 
 app.get('/', (req, res) => {
     res.status(200).send('Curso de Node');
-})
+});
 
 app.get('/livros', (req, res) => {
     res.status(200).json(livros);
-})
+});
+
+app.get('/livros/:id', (req, res) => {
+    let idLivro = parseInt(req.params.id);
+    let index = buscaLivro(idLivro);
+
+    res.json(livros[index]);
+});
 
 app.post('/livros', (req, res) => {
     livros.push(req.body);
     res.status(201).send('Livro foi cadastrado com sucesso!');
 })
+
+app.put('/livros/:id', (req, res) => {
+    let idLivro = parseInt(req.params.id);
+    let index = buscaLivro(idLivro);
+    livros[index].titulo = req.body.titulo;
+
+    res.json(livros);
+});
+
+app.delete('/livros/:id', (req, res) => {
+    let {id} = req.params;
+    let index = buscaLivro(parseInt(id));
+
+    livros.splice(index, 1);
+
+    res.send(`Livro ${id} removido com sucesso!`);
+})
+
+function buscaLivro(id : number){
+    return livros.findIndex(livro => livro.id === id);
+};
 
 export default app;
